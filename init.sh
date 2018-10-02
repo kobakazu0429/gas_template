c2373de11a22ec7448325896f88ec4da2572ad6a
kobakakzu0429 <kobakazu10@gmail.com>
2018-10-02 20:45:29 +0900

    when does not exsit, copy

--- init.sh
+++ init.sh
@@ -1,31 +1,44 @@
 DIR=$(basename $(dirname $(pwd)))
 
-if [! -f '.clasp.template.json'] && [! -f'.clasp.json']; then
+if [ ! -f '.clasp.template.json' ] && [ ! -f '.clasp.json' ]; then
   cp .clasp.template.json ../
+else
+  echo '.clasp.templat exist.'
 fi
 
-if [! -f '.gitignore']; then
+if [ ! -f '.gitignore' ]; then
   cp .gitignore ../
+else
+  echo '.gitignore exist.'
 fi
 
-if [! -f 'package.json']; then
+if [ ! -f 'package.json' ]; then
   cp package.json ../
+else
+  echo 'package.json exist.'
 fi
 
-if [! -f 'yarn.lock']; then
+if [ ! -f 'yarn.lock' ]; then
   cp yarn.lock ../
+else
+  echo 'yarn.lock exist.'
 fi
 
-if [! -f 'tslint.json']; then
+if [ ! -f 'tslint.json' ]; then
   cp tslint.json ../
+else
+  echo 'tslint.json exist.'
 fi
 
-if [! -d 'src/']; then
+if [ ! -d 'src/' ]; then
   cp -r src ../
+else
+  echo 'src/ exist.'
 fi
 
-
-if [ -d '.git/' ]; then
-  cd ../
+cd ../
+if [ -d '.git' ]; then
   git remote set-url origin git@github.com:kobakazu0429/${DIR}.git
+else
+  echo 'not managed by Git'
 fi
